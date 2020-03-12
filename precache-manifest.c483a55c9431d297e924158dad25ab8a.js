07b3a720d9971a29f68fd4aaf83554beb77d6aad
Frank Moran <info@francisfrankmoran.com>
2020-03-11 20:23:26 -0400

    Updates

--- precache-manifest.c483a55c9431d297e924158dad25ab8a.js
+++ precache-manifest.c483a55c9431d297e924158dad25ab8a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f88641bf32f2a5cdc261211b9dee842",
+    "revision": "1ed2377acba5018915bda5f371c8ba75",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "8c132f0f18583cab15b1",
+    "revision": "a09131b8b42d78f39393",
     "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "c29b329a3702046a13ab",
-    "url": "/react-portfolio/static/js/2.0a7dc158.chunk.js"
+    "revision": "8f29f3d009a9fd9f2e48",
+    "url": "/react-portfolio/static/js/2.9e0c3eb6.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/react-portfolio/static/js/2.0a7dc158.chunk.js.LICENSE.txt"
+    "url": "/react-portfolio/static/js/2.9e0c3eb6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8c132f0f18583cab15b1",
-    "url": "/react-portfolio/static/js/main.f3e2e571.chunk.js"
+    "revision": "a09131b8b42d78f39393",
+    "url": "/react-portfolio/static/js/main.507c42d4.chunk.js"
   },
   {
     "revision": "6c1a97d7eedcd4b749e8",
