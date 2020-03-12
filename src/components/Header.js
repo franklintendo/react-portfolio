402bb4b09157f6ff1fd32f303210df1725283283
Frank Moran <info@francisfrankmoran.com>
2020-03-11 21:13:58 -0400

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
     return(
         <nav className="navbar navbar-expand-lg">
-            <Link className="navbar-brand" to="/">Frank Moran</Link>
+            <Link className="navbar-brand" to="/react-portfolio/">Frank Moran</Link>
             
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation-links" aria-controls="navigation-links" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"><i className="fas fa-bars mt-1"></i></span>
@@ -12,13 +12,13 @@ function Header() {
             <div className="collapse navbar-collapse justify-content-end" id="navigation-links">
                 <div className="navbar-nav">
                   
-                        <Link className="nav-item nav-link" to="/">About Me</Link>
+                        <Link className="nav-item nav-link" to="/react-portfolio/">About Me</Link>
 
         
-                        <Link className="nav-item nav-link" to="/portfolio">Portfolio</Link>
+                        <Link className="nav-item nav-link" to="/react-portfolio/portfolio">Portfolio</Link>
                       
                    
-                        <Link className="nav-item nav-link" to="/contact">Contact</Link>
+                        <Link className="nav-item nav-link" to="/react-portfolio/contact">Contact</Link>
                    
                 </div>
             </div>
